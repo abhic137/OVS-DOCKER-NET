c186007c3acf6067839d7aadbfff9aba276d2554
subhrendu1987 <subhrendu@idrbt.ac.in>
2023-06-12 08:53:32 +0530

    modified:   createLink.sh

--- createLink.sh
+++ createLink.sh
@@ -2,8 +2,26 @@
 ## sudo bash createLink.sh <C1_name> <C2_Name>
 C1_NAME=$1
 C2_NAME=$2
+##############################################################
+createNS(){
+	CONTAINER_ID=$1
+	pid=$(docker inspect -f '{{.State.Pid}}' ${CONTAINER_ID})
+	mkdir -p /var/run/netns/
+	ln -sfT /proc/$pid/ns/net /var/run/netns/${CONTAINER_ID})
+	#ip netns exec ${CONTAINER_ID}) ip a
+	echo "Namespace Created for "${CONTAINER_ID}
+}
+#################################
+addLink(){
+	C1_ID=$1
+	C2_ID=$2
+	#ip netns exec ${CONTAINER_ID}) ip a
+	echo "Namespace Created for "${CONTAINER_ID}
+}
+##############################################################
+C1_ID=$(docker inspect --format="{{.Id}}" ${C1_NAME})
+createNS(C1_ID)
+C2_ID=$(docker inspect --format="{{.Id}}" ${C2_NAME})
+createNS(C2_ID)
 
-pid=$(docker inspect -f '{{.State.Pid}}' ${container_id})
-mkdir -p /var/run/netns/
-ln -sfT /proc/$pid/ns/net /var/run/netns/${container_id})
-ip netns exec ${container_id}) ip a
+addLink(C1_ID,C2_ID)

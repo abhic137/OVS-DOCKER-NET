1928ea867ff382c5bae5e49b126ae1752fc34ef7
subhrendu1987 <subhrendu@idrbt.ac.in>
2023-06-12 12:13:10 +0530

    modified:   createLink.sh

--- createLink.sh
+++ createLink.sh
@@ -7,30 +7,30 @@ createNS(){
 	C_ID=$1
 	#echo "Creating namespace for ${C_ID}"
 	pid=$(docker inspect -f '{{.State.Pid}}' ${C_ID})
-	CNAME=$(docker inspect --format='{{.Name}}' ${C_ID})
+	CNAME=$(basename $(docker inspect --format='{{.Name}}' ${C_ID}))
 	echo "Name=${CNAME}, PID=${pid}"
 	mkdir -p /var/run/netns/
 	ln -sfT /proc/$pid/ns/net /var/run/netns/${CNAME}
-	#ip netns exec ${CONTAINER_ID}) ip a
+	#ip netns exec ${CNAME} ip a
 	echo "Namespace ${CNAME} Created for "${C_ID}
 }
 #################################
 addLink(){
-	C1_ID=$1
-	C2_ID=$2
+	C1=$1
+	C2=$2
 	#ip netns exec ${CONTAINER_ID}) ip a
 	echo "Creating Link ..."
 	ip link add veth1_l type veth peer veth1_r
-	ip link set veth1_l netns ${C1_ID}
-	ip link set veth1_r netns ${C2_ID}
-	ip netns exec ${C1_ID} ip l set veth1_l name eth0
-	ip netns exec ${C2_ID} ip l set veth1_r name eth0
-	ip netns exec ${C1_ID} ip a add 10.0.0.1/30 dev eth0
-	ip netns exec ${C2_ID} ip a add 10.0.0.2/30 dev eth0
-	ip netns exec ${C1_ID} ip l set eth0 up
-	ip netns exec ${C2_ID} ip l set eth0 up
-	ip netns exec ${C1_ID} ip r add 0.0.0.0/0 via 10.0.0.2
-	ip netns exec ${C2_ID} ip r add 0.0.0.0/0 via 10.0.0.1
+	ip link set veth1_l netns ${C1}
+	ip link set veth1_r netns ${C2}
+	ip netns exec ${C1} ip l set veth1_l name eth0
+	ip netns exec ${C2} ip l set veth1_r name eth0
+	ip netns exec ${C1} ip a add 10.0.0.1/30 dev eth0
+	ip netns exec ${C2} ip a add 10.0.0.2/30 dev eth0
+	ip netns exec ${C1} ip l set eth0 up
+	ip netns exec ${C2} ip l set eth0 up
+	ip netns exec ${C1} ip r add 0.0.0.0/0 via 10.0.0.2
+	ip netns exec ${C2} ip r add 0.0.0.0/0 via 10.0.0.1
 }
 ##############################################################
 C1_ID=$(docker inspect --format="{{.Id}}" ${C1_NAME})
@@ -38,7 +38,7 @@ createNS ${C1_ID}
 C2_ID=$(docker inspect --format="{{.Id}}" ${C2_NAME})
 createNS ${C2_ID}
 ##########
-addLink ${C1_ID} ${C2_ID}
+addLink ${C1_NAME} ${C2_NAME}
 
 
 
